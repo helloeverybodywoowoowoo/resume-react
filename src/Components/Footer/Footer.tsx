ab1a96b82de062c640522c41cc0d8c9dc436be98
Kashope Akinsade <akinsadekash@gmail.com>
2025-07-30 18:36:56 -0400

    added footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <>
             <div>
-                <p>&copy; {new Date().getFullYear()} K.A. - akinsadekash@gmail.com - =0]</p>
+                <p>&copy; {new Date().getFullYear()} K.A. - akinsadekash@gmail.com - ♡</p>
             </div>
         </>
     )
