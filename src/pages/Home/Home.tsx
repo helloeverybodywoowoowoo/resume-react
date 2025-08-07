2ca99d0393698bdb37ffb18d6b47cbab2e75d1e9
Kashope Akinsade <akinsadekash@gmail.com>
2025-08-07 17:11:59 -0400

    home page fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,11 @@
 function Home(){
-    return <h1> H o m e P a g e </h1>
+    return (
+        <>
+            <h1> H o m e P a g e </h1>
+            <li style={{ textAlign: "center", marginTop: "12px" }}>This is a website that I made (with alot of help) over the last couple months. I hope you like it.</li>
+            
+        </>
+    );
 }
 
 export default Home;
