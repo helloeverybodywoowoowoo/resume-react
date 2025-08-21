e7b73035685d42af689d6765a03242026801e917
Kashope Akinsade <akinsadekash@gmail.com>
2025-08-21 07:36:43 -0400

    fixing resume n ecom page

--- src/pages/Ecom/Ecom.tsx
+++ src/pages/Ecom/Ecom.tsx
@@ -6,6 +6,8 @@ function Ecom(){
     <div>
     <h1 style={{ textAlign: "center", marginTop: "24px" }}> The Sims Reading List </h1>
     <li style={{ textAlign: "center", marginTop: "12px" }}>Here are some titles that might enhance your understanding of some of the background and social issues entertained in The Sims. Warning: all are filled with provocative ideas; Maxis disavows any responsibility for encouraging deep thought.</li>    
+    <li style={{ textAlign: "center", marginTop: "12px" }}>In one of my favorite game series, The Sims, The Sims 1 originally came with a list of recommend reading, so don't even THINK about loading into Create-a-Sim until you've read Notes on the Synthesis of Form by Christopher W. Alexander</li>    
+   
     <ProductGrid />
     </div>
     );
--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -12,13 +12,72 @@ function Resume() {
 
         </header>
         <section className="contact-info">
-          <p><strong>Email:</strong> akinsadekash@gmail.com <strong> Phone:</strong> (240)-360-7625<strong> Github:</strong> https://tinyurl.com/5t4y63d3</p>
+          <p><strong>Email:</strong> akinsadekash@gmail.com <strong> Phone:</strong> (240)-360-7625<strong> Linkedin:</strong> www.linkedin.com/in/kashope-akinsade/<strong> Github:</strong> https://tinyurl.com/5t4y63d3</p>
         </section>
         <section className="resume-section">
           <h2>Professional Summary</h2>
           <p>
-            Full-stack JavaScript developer with 24 weeks of immersive, hands-on training at Claim Academy. Experienced in creating responsive, user-focused applications using modern technologies like React, Node.js, and Express.
-          </p>
+            Resourceful and results-driven full-stack JavaScript developer with a strong foundation in data analysis and backend architecture. Known for transforming abstract ideas into intuitive, scalable applications—whether building dynamic React interfaces or architecting secure APIs with Node.js and MongoDB. Recent projects showcase a flair for interactive UI features and persistent data integration, reflecting a balance of creativity and precision.</p>
+        </section>
+
+        <section className="resume-section">
+          <h2>Education</h2>
+          <ul>
+            <li><strong>University of Maryland, Baltimore County (UMBC):</strong> B.S. in Information Systems (Completed 2022)</li>
+            <li><strong>Additional Courses:</strong> MongoDB Integration, TypeScript Deep Dive</li>
+          </ul>
+        </section>
+
+        <section className="resume-section">
+          <h2>Professional Experience</h2>
+            <div style={{ marginBottom: '1rem' }}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <strong>Claim Academy – Freelance Developer</strong>
+                <span>Feb 2025 – Present</span>
+              </div>
+              <ul style={{ marginLeft: '2rem' }}>
+                <li>Built over 10+ responsive React interfaces with reusable components</li>
+                <li>Developed 4 RESTful APIs using Express.js and Node.js</li>
+                <li>Integrated MongoDB for dynamic data storage and retrieval</li>
+                <li>Implemented user authentication and protected routes</li>
+              </ul>
+            </div>
+
+            <div style={{ marginBottom: '1rem' }}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <strong>Stoners Ford Consulting, Inc. – Technical Support Analyst</strong>
+                <span>Jan 2025 – Jun 2025</span>
+              </div>
+              <ul style={{ marginLeft: '2rem' }}>
+                <li>Diagnosed and resolved hardware, software, and network issues across end-user systems</li>
+                <li>Provided tiered technical assistance via phone, email, and remote tools maintaining a 95% resolution rate within SLA timeframes</li>
+                <li>Maintained support documentation and collaborated with IT teams, helping streamline IT workflows and boost system reliability by 15%</li>
+              </ul>
+            </div>
+
+            <div style={{ marginBottom: '1rem' }}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <strong>Quality Associates Inc. – Document Prep Specialist</strong>
+                <span>Aug 2024 – Dec 2024</span>
+              </div>
+              <ul style={{ marginLeft: '2rem' }}>
+                <li>Scanned, digitized, and organized thousands of documents for accurate data entry</li>
+                <li>Used software tools to categorize and index documents, generating processing reports</li>
+              </ul>
+            </div>
+
+            <div style={{ marginBottom: '1rem' }}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <strong>MADEL LLC – Data Analyst</strong>
+                <span>Feb 2023 – Sep 2023</span>
+              </div>
+              <ul style={{ marginLeft: '2rem' }}>
+                <li>Organized, cleaned, and stored data in relational databases</li>
+                <li>Mined data from selected sources for analysis</li>
+                <li>Performed basic analytics using MS Power BI and Excel</li>
+              </ul>
+            </div>
+
         </section>
 
         <section className="resume-section">
@@ -33,7 +92,27 @@ function Resume() {
           </ul>
         </section>
 
-        <section className="resume-section two-column">
+        <section className="resume-section">
+          <h2>Soft Skills</h2>
+          <ul className="skills-list">
+            <li>Attention to Detail	</li>
+            <li>Communication</li>
+            <li>Problem-solving</li>
+            <li>Critical Thinking</li>
+            <li>Adaptability</li>
+            <li>Creativity</li>
+          </ul>
+        </section>
+
+        <section className="resume-section">
+          <h2>Certifications</h2>
+          <ul className="skills-list">
+            <li>AWS Certified Cloud Practitioner</li>
+            <li>AWS Certified SysOps Administrator Associate </li>
+          </ul>
+        </section>
+
+        {/* <section className="resume-section two-column">
           <div className="column">
             <h2>Education</h2>
             <ul>
@@ -50,7 +129,7 @@ function Resume() {
               <li><strong>MADEL LLC. - Data Analyst </strong> Applied concepts in Statistical Data Analysis and Computer Information Systems to improve reporting </li>
             </ul>
           </div>
-        </section>
+        </section> */}
 
         {/* <section className="resume-section">
           <h2>Bootcamp Coursework</h2>
